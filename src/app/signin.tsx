eaf514c3cc04dabf4a94959fa1e0e0d7d5f4a8b9
habibmimouna <habibmimouna@gmail.com>
2023-02-15 01:05:43 +0100

    add signup & signin form

--- src/app/signin.tsx
+++ src/app/signin.tsx
@@ -1,20 +1,39 @@
 import styles from './page.module.css'
 
+export const SignIn = () => {
+    const showUser = (e: any) => {
+        e.preventDefault()
+        const target = e.target as typeof e.target & {
+            email: { value: string };
+            password: { value: string };
+        };
+        const email = target.email.value;
+        const password = target.password.value;
+        console.log(email, ' ', password)
+        const userdata = {
+            password,
+            email
+        };
+    console.log(userdata);
+    
 
+    }
+    return (
+        <main className={styles.main}>
+            <form onSubmit={(e) => showUser(e)} method="post">
+                <h1>Login Sucker</h1>
+                Email<input type="text" title='Email' className={styles.element} id="email" />
+                Password<input type="password" title='password' className={styles.element} id="password" />
+                <button type="submit">LOG IN</button>
+                <div className={styles.elements}>
+                    <h3>don't have an account create one</h3>
+            
+                </div>
+            </form>
+            <a href='sign-up'>
+                        <button  >Sign up</button>
+                    </a>
+        </main>
 
-export const SignIn = () =>{
-  return (
-    <main className={styles.main}>
-      <h1>Login Sucker</h1>
-      Email<input type="text" title='Email' className={styles.element}/>
-      Password<input type="password" title='password'  className={styles.element} />
-      <button>LOG IN</button>
-      <div className={styles.elements}>
-       <h3>don't have an account create one</h3>
-       <a href='sign-up'>
-       <button  >Sign up</button>
-       </a>
-      </div>
-    </main>
-  )
+    )
 }
--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -3,14 +3,29 @@ import styles from './page.module.css'
 
 
 export const Signup = () =>{
+  const makeUser = (e: any) => {
+    e.preventDefault()
+    const target = e.target as typeof e.target & {
+        username:{value: string};
+        email: { value: string };
+        password: { value: string };
+    };
+    const username = target.username.value;
+    const email = target.email.value;
+    const password = target.password.value;
+    console.log(username,'',email, ' ', password)
+}
   return (
     <main className={styles.main}>
+      <form onSubmit={(e) => makeUser(e)} method="post">
       <h1>Login Sucker</h1>
-      Username<input type="text" title='Email' className={styles.element}/>
-      Email<input type="text" title='Email' className={styles.element}/>
-      Password<input type="password" title='password'  className={styles.element} />
+      Username<input type="text" title='Email' className={styles.element} id='username'/>
+      Email<input type="text" title='Email' className={styles.element} id='email'/>
+      Password<input type="password" title='password'  className={styles.element} id='password' />
       Confirm Password<input type="password" title='password'  className={styles.element} />
-      <button>SIGN IN</button>
+      <button type='submit'>SIGN IN</button>
+      
+      </form>
       <div className={styles.elements}>
        <h3>have an account </h3>
        <a href='sign-in'>
